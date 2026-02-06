69fcc37ceb743784382ac9c8be849b0b66ae0e4e
rickysaarloos <9027376@student.zadkine.nl>
2026-02-06 14:14:55 +0100

    hoi

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+
 import Header from "./Header";
 import Footer from "./Footer";
-import "../App.css";
-import "./Layout.css";
-
 
 const Layout = ({ children }) => {
   return (
-    <div className="page-container">
-   
-<a 
-  href="https://github.com/rickysaarloos"
-  target="_blank"
-  rel="noopener noreferrer"
-  className="github-logo"
->
-  <img 
-    src="./images/pngimg.com - github_PNG40.png" 
-    alt="GitHub" 
-  />
-</a>
-      <main className="content">{children}</main>
+    <div className="relative min-h-screen flex flex-col bg-[#e5e5e5]">
+      
+      {/* GitHub floating logo */}
+      <a
+        href="https://github.com/rickysaarloos"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed top-6 right-6 z-50 w-12 h-12 hover:scale-105 transition"
+      >
+        <img
+          src="./images/pngimg.com - github_PNG40.png"
+          alt="GitHub"
+          className="w-full h-full object-contain"
+        />
+      </a>
+
+      {/* Optional header */}
+      <Header />
+
+      {/* Page content */}
+      <main className="flex-1">
+        {children}
+      </main>
+
+      {/* Footer always at bottom */}
       <Footer />
     </div>
   );
